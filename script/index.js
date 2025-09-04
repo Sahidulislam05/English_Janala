32fad86824da399b8fa05d2abafc0d868b48a4d4
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-09-04 09:32:22 +0600

    Added english janala

---
+++ script/index.js
@@ -0,0 +1,6 @@
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((json) => console.log(json.data));
+};
+loadLessons();
