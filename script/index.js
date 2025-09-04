4c5df6be3dac58c5fc7daa8348640a120b2067e8
Sahidulislam05 <sahidulislamcst@gmail.com>
2025-09-04 21:28:58 +0600

    Added modal

--- script/index.js
+++ script/index.js
@@ -20,6 +20,63 @@ const loadLevelWord = (id) => {
     });
 };
 
+// {
+//     "word": "Fascinate",
+//     "meaning": "মুগ্ধ করা",
+//     "pronunciation": "ফ্যাসিনেট",
+//     "level": 5,
+//     "sentence": "The magician's tricks fascinate the audience.",
+//     "points": 4,
+//     "partsOfSpeech": "verb",
+//     "synonyms": [
+//         "captivate",
+//         "charm",
+//         "enchant"
+//     ],
+//     "id": 6
+// }
+
+const loadWordDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+};
+
+const displayWordDetails = (word) => {
+  console.log(word);
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `
+
+    <div>
+            <h2 class="text-2xl font-bold">
+              ${word.word} (<i class="fa-solid fa-microphone-lines"></i>: ${word.pronunciation})
+            </h2>
+          </div>
+          <div>
+            <h2 class="font-bold">Meaning</h2>
+            <p>${word.meaning}</p>
+          </div>
+          <div>
+            <h2 class="font-bold">Example</h2>
+            <p>${word.sentence}</p>
+          </div>
+          <div>
+            <h2 class="font-bold">সমার্থক শব্দ গুলো</h2>
+            <span class="btn">Enthusiastic</span>
+            <span class="btn">Enthusiastic</span>
+            <span class="btn">Enthusiastic</span>
+          </div>
+  
+  
+  
+  
+  
+  
+  `;
+  document.getElementById("word_modal").showModal();
+};
+
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
@@ -61,7 +118,9 @@ const displayLevelWord = (words) => {
       word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায় নি"
     }"</div>
         <div class="flex justify-between items-center">
-          <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+          <button onclick="loadWordDetail(${
+            word.id
+          })" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
             <i class="fa-solid fa-circle-info"></i>
           </button>
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
